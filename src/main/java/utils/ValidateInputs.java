518e81201d46462bd73e76a4838bfeaf9e3b8325
Jusep1983 <j.lopezcalvo1983@gmail.com>
2025-05-20 13:52:13 +0200

    feat: add temporary main menu

--- src/main/java/utils/ValidateInputs.java
+++ src/main/java/utils/ValidateInputs.java
@@ -64,7 +64,7 @@ public class ValidateInputs {
         }
     }
 
-    public static int readIntegerBetweenOnRange(String message, int minimum, int maximum) {
+    public static int validateIntegerBetweenOnRange(String message, int minimum, int maximum) {
         while (true) {
             try {
                 System.out.print(message);
@@ -79,6 +79,28 @@ public class ValidateInputs {
                 System.out.println("Error, " + e.getMessage());
             }
         }
+    }
+
+    // Menu provisional
+    public static int mainMenuOptions() {
+        return validateIntegerBetweenOnRange(""" 
+                
+                MAIN ESCAPE ROOM MENÚ
+                ---Editor options-------
+                1.  Show total inventory
+                2.  Show total inventory value €
+                3.  Add room
+                4.  Delete room
+                5.  Edit room (add/del clue and decoration)
+                ---Business options-----
+                6.  Generate tickets (elegir sala, ++€ totalSales)
+                7.  Show total sales
+                ---Users options--------
+                8.  Notifications
+                9.  Unsubscribe players
+                10. Generate certificate
+                0.  Exit
+                """, 0, 10);
     }
 
 }
