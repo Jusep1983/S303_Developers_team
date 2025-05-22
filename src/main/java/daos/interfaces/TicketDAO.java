2dd7e23a7dc82c0e3a634793e948c754ce95c6ed
Luis Portas <otrorotopo@gmail.com>
2025-05-22 13:30:29 +0200

    fix: psmall conflicts ):

--- src/main/java/daos/interfaces/TicketDAO.java
+++ src/main/java/daos/interfaces/TicketDAO.java
@@ -2,12 +2,13 @@ package daos.interfaces;
 
 import entities.Ticket;
 import org.bson.types.ObjectId;
+import org.bson.Document;
 
 public interface TicketDAO {
 
     void save(Ticket ticket);
     Ticket findById(ObjectId id);
     void deleteById(ObjectId id);
-    void getPrice(ObjectId id);
+    int getPrice(Document doc);
     int count();
 }
