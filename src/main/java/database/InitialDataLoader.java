ab294888008e9a0129cdb6ca7734889cb2c00252
zohra-b <z.bellamine@gmail.com>
2025-05-23 16:46:23 +0200

    feat : added rooms.json + class InitialDataLoader

---
+++ src/main/java/database/InitialDataLoader.java
@@ -0,0 +1,34 @@
+package database;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class InitialDataLoader {
+    public static void loadInitialRoomsIfDatabaseIsEmpty(MongoDatabase database, String jsonFilePath ) {
+        MongoCollection<Document> roomsCollection = database.getCollection("rooms");
+
+        if (roomsCollection.countDocuments() != 0) {
+            System.out.println("Skipping initial data load.");
+        } else {
+            try {
+                String jsonFile = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+                List<Document> documents = Document.parse("{\"array\":" + jsonFile + "}").getList("array", Document.class);
+
+                roomsCollection.insertMany(documents);
+                System.out.println("Initial data inserted into 'rooms' collection.");
+
+            } catch (Exception e) {
+                System.out.println("Error loading initial data: " + e.getMessage());
+            }
+        }
+    }
+
+
+    }
+
