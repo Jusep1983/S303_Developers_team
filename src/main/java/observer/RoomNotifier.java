5209507763f220f5e3b17310b85cf4121618b3ad
Luis Portas <otrorotopo@gmail.com>
2025-05-28 09:54:03 +0200

    refactor: moved notifying methods to RoomNotifier

--- src/main/java/observer/RoomNotifier.java
+++ src/main/java/observer/RoomNotifier.java
@@ -1,13 +1,17 @@
 package observer;
 
 import daos.PlayerDAOImpl;
+import entities.Newsletter;
 import entities.Player;
 import entities.Room;
+import utils.ValidateInputs;
 
 import java.util.List;
 
 public class RoomNotifier {
     private final PlayerDAOImpl playerDAO = new PlayerDAOImpl();
+    private final List<Player> players = new PlayerDAOImpl().findAll();
+    private final Newsletter newsletter = new Newsletter(new PlayerDAOImpl().findAll());
 
     public void notifyAllSubscribedPlayers(Room room) {
         List<Player> players = playerDAO.findAll();
@@ -16,5 +20,30 @@ public class RoomNotifier {
                 p.update("A new room has been created! Try our new " + room.getName());
             }
         }
+    }
+
+    public void notifyAllPlayers() {
+        String lastNewsToAll = ValidateInputs.validateString("Enter the message you want to send to all players : ");
+        newsletter.setLastNews(lastNewsToAll);
+        newsletter.getObservers().clear();
+        for (Player player : players) {
+            newsletter.addObserver(player);
+        }
+        newsletter.notifyObservers();
+        //newsletter.notifyAllPlayers();
+        System.out.println("Notification sent to all players!");
+    }
+
+    public void notifySubscribedPlayers() {
+        String lastNewsToSubscribed = ValidateInputs.validateString("Enter the message you want to send to your subscribers : ");
+        newsletter.setLastNews(lastNewsToSubscribed);
+        newsletter.getObservers().clear();
+        for (Player player : players) {
+            if (player.isSubscribed())
+                newsletter.addObserver(player);
+        }
+        newsletter.notifyObservers();
+        //newsletter.notifySubscribed();
+        System.out.println("Notification sent to subscribed players!");
     }
 }
